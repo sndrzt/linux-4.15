c6d31e6ad03215286bcfe8ad8a54034693f2f61a
Sndrz Teo <sndrz@outlook.com>
2024-06-07 19:43:19 +0800

    remove unused files
    Signed-off-by: Sndrz Teo <sndrz@outlook.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+sudo apt-get update
+sudo apt-get install -y net-tools openssh-server vim dpkg-dev build-essential bison flex libncursesw5-dev libssl-dev git tig
+sudo apt-get build-dep -y linux-source-4.15.0
+
 make menuconfig
 
 date > a.txt
@@ -10,4 +14,9 @@ date > b.txt
 
 sudo make modules_install
 sudo make install
+
+udevadm info /dev/iio:device0
+echo -e "sensor:modalias:acpi:BOSC0200*:dmi:*\n ACCEL_MOUNT_MATRIX=0,1,0;-1,0,0;0,0,-1" > 61-sensor-local.hwdb
+sudo cp 61-sensor-local.hwdb /etc/udev/hwdb.d/
+sudo systemd-hwdb update
 
